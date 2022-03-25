88db7925a49120296e175025940827f61b28793f
me <me>
2022-03-25 11:31:47 -0700

    turtle mock works

--- display/turtle_display.py
+++ display/turtle_display.py
@@ -13,9 +13,11 @@ class TurtleDisplay(Display):
 
 	def draw(self) -> None:
 		for x in range(0, self.screen.WIDTH):
+			print(f"x: {x}")
 			self.hertz = 2 if self.hertz < 2 else self.hertz # minimum hz value. should go from 2-28. i forgot why - ssohbn
 			y = wave_engine.calculate_wave_point(self.screen, x, self.hertz, wave_type=WaveType.SINE)
 
-		print(f"x: {x}, y: {y}")
-		self.turtle.goto(x-100, y)
-		self.turtle.pendown()
+			print(f"x: {x}, y: {y}")
+			self.turtle.goto(x-100, y)
+			self.turtle.pendown()
+
