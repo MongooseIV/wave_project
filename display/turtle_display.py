438dc5c046453931c7876e83b90fd6eb73ad83f9
me <me>
2022-03-25 11:52:30 -0700

    turtle mock is working

--- display/turtle_display.py
+++ display/turtle_display.py
@@ -1,23 +1,29 @@
+from tracemalloc import start
 from turtle import Turtle
+import turtle
 
 from display.screen import Display, Size
 import wave_engine
 from wave_engine import WaveType
 
 class TurtleDisplay(Display):
-	def __init__(self, screen: Size, hertz: int):
+	def __init__(self, screen: Size, hertz: int, start_x_offset: int):
 		self.screen = screen
 		self.hertz = hertz
+		# offset because turtle decides to start in the middle
+		self.start_x_offset = start_x_offset 
 
 		self.turtle = Turtle()
+		self.turtle.speed(0)
+		self.turtle.hideturtle()
+
 
 	def draw(self) -> None:
+		turtle.tracer(0,0)
 		for x in range(0, self.screen.WIDTH):
-			print(f"x: {x}")
-			self.hertz = 2 if self.hertz < 2 else self.hertz # minimum hz value. should go from 2-28. i forgot why - ssohbn
+			# self.hertz = 2 if self.hertz < 2 else self.hertz # minimum hz value. should go from 2-28. i forgot why - ssohbn
 			y = wave_engine.calculate_wave_point(self.screen, x, self.hertz, wave_type=WaveType.SINE)
 
-			print(f"x: {x}, y: {y}")
-			self.turtle.goto(x-100, y)
+			self.turtle.goto(x+self.start_x_offset, y)
 			self.turtle.pendown()
 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # from display.oled_display import OledDisplay # TODO: apparently i dont understand how modules work in python
+from time import sleep
+import turtle
 from display.turtle_display import TurtleDisplay
 
 from display.screen import Size
@@ -6,7 +8,24 @@ from wave_engine import WaveType
 # group width and height
 
 # display = OledDisplay (Size(128, 32) )
-display = TurtleDisplay( Size(128, 32), 12 )
+display = TurtleDisplay( Size(500, 300), 18, -400 )
+
+def draw_thingy(hz: int):
+	sleep(0.05)
+	display.turtle.clear()
+	display.hertz = hz
+	display.draw()
+	turtle.update()
+
+	display.turtle.penup()
+	display.turtle.goto(display.start_x_offset, 0)
+	display.turtle.pendown()
+
 
 while True:
-	display.draw()
+
+	for i in range(2, 28):
+		draw_thingy(i)
+
+	for i in reversed(range(2, 28)):
+		draw_thingy(i)
