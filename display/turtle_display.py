37332f6a985000022d81728282306f0e44d9f84c
me <me>
2022-03-28 13:07:35 -0700

    fix turtle thing

--- display/turtle_display.py
+++ display/turtle_display.py
@@ -1,6 +1,7 @@
 from tracemalloc import start
 from turtle import Turtle
 import turtle
+from time import sleep
 
 from display.screen import Display, Size
 import wave_engine
@@ -17,8 +18,14 @@ class TurtleDisplay(Display):
 		self.turtle.speed(0)
 		self.turtle.hideturtle()
 
+		self.turtle.penup()
+		self.turtle.goto(self.start_x_offset, 0)
+		self.turtle.pendown()
 
 	def draw(self) -> None:
+		sleep(0.05)
+		self.turtle.clear()
+
 		turtle.tracer(0,0)
 		for x in range(0, self.screen.WIDTH):
 			# self.hertz = 2 if self.hertz < 2 else self.hertz # minimum hz value. should go from 2-28. i forgot why - ssohbn
@@ -26,4 +33,6 @@ class TurtleDisplay(Display):
 
 			self.turtle.goto(x+self.start_x_offset, y)
 			self.turtle.pendown()
+
+		turtle.update()
 
